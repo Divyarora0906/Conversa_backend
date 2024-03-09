a09d6a6e48fb416e36bc100a167492c6c6bf8d14
Divy Arora <126507438+Divyarora0906@users.noreply.github.com>
2024-03-09 17:55:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,23 +26,7 @@ app.get("/api/chats", (req, res) => {
 app.use('/api/user',userRoutes)
 app.use('/api/chat',chatRoutes)
 app.use('/api/message',messageRoutes)
-// -------------------------Deployment-------------------------------
-const __dirname1 = path.resolve();
-if(process.env.NODE_ENV === "production"){
-   app.use(express.static(path.join( __dirname1,'/Client/dist')));
-   app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname1,"Client","dist","index.html"))
-   })
-}else{
-  app.get("/",(req,res)=>{
-    res.send("API is running Successfully");
-  })
-}
 
-
-
-
-// -------------------------Deployment-------------------------------
 
 
 
